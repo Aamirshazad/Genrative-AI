078b4277fdd9562edb298765c4efc2667f418972
Aamirshazad <amiralicomsats3@gmail.com>
2024-07-22 11:42:31 +0500

    add new functions

--- genrative_ai/main.py
+++ genrative_ai/main.py
@@ -10,3 +10,10 @@ async def root():
 async def hy1():
     return {"message" : "Hello"}
 
+@app.get("/how/")
+async def hy2():
+    return {"message" : "How are you?"}
+
+@app.get("/bye/")
+async def hy3():
+    return {"message" : "Good Bye"}
